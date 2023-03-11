6ea539f15005a3992fb5d31928e7e0013c0a9648
NarminQ <quliyevanermin05012001@gmail.com>
2023-03-11 11:53:05 +0400

    Renad 010

---
+++ Gr96UnitTesting/Oper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gr96UnitTesting
+{
+    internal class Oper
+    {
+
+        public string GetName()
+        {
+            string name = "Bekir Mehdiyev";
+            return name;
+        }
+
+    }
+}
